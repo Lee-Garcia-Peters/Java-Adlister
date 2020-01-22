9a2f03f56549b422cf43fb3a427c3846f7a3d46e
AlexWonkeunLee <AlexWonkeunLee@gmail.com>
2020-01-22 14:57:12 -0600

    parameters and populating list w/ query

--- src/main/java/com/codeup/adlister/controllers/LocationServlet.java
+++ src/main/java/com/codeup/adlister/controllers/LocationServlet.java
@@ -8,11 +8,16 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "controllers.LocationServlet", urlPatterns = "/location")
 public class LocationServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        session.setAttribute("type", request.getParameter("item"));
+
+        System.out.println((String)session.getAttribute("type"));
         request.getRequestDispatcher("/WEB-INF/location.jsp").forward(request, response);
     }
 }
