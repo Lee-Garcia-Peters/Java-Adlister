f5f876bcbf34155990751219c72a22c40d738094
Joshua Peters <peters.l.joshua@gmail.com>
2020-01-23 14:45:48 -0600

    Saving ad jsp

--- src/main/java/com/codeup/adlister/controllers/AdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/AdServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "controllers.AdServlet", urlPatterns = "/Ad")
@@ -14,6 +15,8 @@ public class AdServlet extends HttpServlet {
             response.sendRedirect("/login");
             return;
         }
+        HttpSession session = request.getSession();
+        session.setAttribute("id",request.getParameter("id"));
         request.getRequestDispatcher("/WEB-INF/Ad.jsp").forward(request, response);
     }
 }
